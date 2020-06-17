e75289de95a14244d8e9dfca49673def9bf530cd
Tounsi <tounsils@gmail.com>
2020-06-18 00:37:40 +0100

    new version

---
+++ index3.php
@@ -0,0 +1,50 @@
+<!doctype html>
+ <link rel="shortcut icon" href="favicon.ico"/> 
+<title>TN76.com Site Maintenance</title>
+<style>
+  body { text-align: center; padding: 150px; }
+  h1 { font-size: 50px; }
+  body { font: 20px Helvetica, sans-serif; color: #333; }
+  article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+  a { color: #dc8100; text-decoration: none; }
+  a:hover { color: #333; text-decoration: none; }
+</style>
+
+<article>
+    <h1>We&rsquo;ll be back soon!</h1>
+    <div>
+        <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:tn76contact@gmail.com">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
+        <p>&mdash; The Team</p>
+		
+		
+		<script>
+  (function() {
+    var cx = 'partner-pub-2507708786892986:4321291414';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
+</script>
+<gcse:searchbox-only></gcse:searchbox-only>
+
+
+
+
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- tn76.com -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-2507708786892986"
+     data-ad-slot="7748512485"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+
+
+    </div>
+</article>
